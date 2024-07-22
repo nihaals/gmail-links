40fd24e6204f4f6d3a551ae249f691a793feaffa
Nihaal Sangha <nihaal.git@gmail.com>
2024-07-22 01:02:06 +0100

    Fix service worker not being deployed

--- cf-pages-build.sh
+++ cf-pages-build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 mkdir -p dist
-sed "s/Version unknown/Version $CF_PAGES_COMMIT_SHA/" index.html > dist/index.html
+mv src/*.{html,js} dist
+sed -i "s/Version unknown/Version $CF_PAGES_COMMIT_SHA/" dist/index.html
