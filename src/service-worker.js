cdc7be59f49a0b92d71b391563e07d2058a73293
Nihaal Sangha <nihaal.git@gmail.com>
2025-06-09 15:36:31 +0100

    Move offline status update to service worker
    This prevents a flash as "Online-only" is modified to "Offline-first" after the page is loaded and makes the status more accurate.

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,32 @@
 const cacheName = "static-v1"
 const precachedAssets = ["/"]
 
+/** @param {Response} response */
+const updateOfflineStatus = async (response) => {
+  const responseText = await response.text()
+  // DOMParser is not available in service workers
+  const newResponseText = responseText.replace(
+    '<span id="offline-status">Online-only</span>',
+    '<span id="offline-status">Offline-first</span>',
+  )
+  return new Response(newResponseText, {
+    headers: response.headers,
+    status: response.status,
+    statusText: response.statusText,
+  })
+}
+
 self.addEventListener("install", (event) => {
   self.skipWaiting()
   event.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName)
       await cache.addAll(precachedAssets)
+      const response = await cache.match("/")
+      if (response) {
+        const modifiedResponse = await updateOfflineStatus(response)
+        await cache.put("/", modifiedResponse)
+      }
     })(),
   )
 })
@@ -34,8 +54,9 @@ self.addEventListener("fetch", async (event) => {
       const cachedResponse = await cache.match(event.request)
       const networkResponse = (async () => {
         const response = await fetch(event.request)
-        await cache.put(event.request, response.clone())
-        return response
+        const modifiedResponse = await updateOfflineStatus(response)
+        await cache.put(event.request, modifiedResponse.clone())
+        return modifiedResponse
       })()
       return cachedResponse || networkResponse
     })(),
